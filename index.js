4687a3c9f838e5388685b90515f63ed544075885
MariiaUlankina <ulankina.masha@gmail.com>
2021-12-02 17:57:19 +0200

    change index.js

--- index.js
+++ index.js
@@ -2,13 +2,26 @@ let first_num = prompt ('Enter first number');
 let second_num = prompt ('Enter second number');
 let operation = prompt ('Enter operation');
 
-function summ(operation){
-    if (operation == '+'){
-        let result = Number(first_num) + Number(second_num);
-        let showMessage = alert (`Результат: ${first_num} ${operation} ${second_num} = ${result}`) 
-    }
+
+function plus(){
+     const result = Number(first_num) + Number(second_num);
+     console.log(`Результат: ${first_num} + ${second_num} = ${result}`);
+}
+
+function minus(){
+       const result = Number(first_num) - Number(second_num);
+       console.log(`Результат: ${first_num} - ${second_num} = ${result}`);
+}
+
+function multiplication(){
+       const result = Number(first_num) * Number(second_num);
+       console.log(`Результат: ${first_num} * ${second_num} = ${result}`);
+}
+
+function division (){
+       const result = Number(first_num) / Number(second_num);
+       console.log(`Результат: ${first_num} / ${second_num} = ${result}`);
 }
 
 
 
-console.log(summ)
